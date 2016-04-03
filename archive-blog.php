39ecd0ba1fb86d3cca1fa69c8d906f0b5ed7d448
sneffadi <sneffadi@gmail.com>
2016-04-03 15:01:13 -0600

    updated blog post type and sidebar

--- archive-blog.php
+++ archive-blog.php
@@ -20,28 +20,34 @@ get_header(); ?>
 
 <div id="page" role="main">
 	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
+	<div class="row">
+		<div class="small-24 medium-18 columns">
+			<?php if ( have_posts() ) : ?>
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'content', get_post_format() ); ?>
-		<?php endwhile; ?>
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', get_post_format() ); ?>
+			<?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'content', 'none' ); ?>
+			<?php else : ?>
+				<?php get_template_part( 'content', 'none' ); ?>
 
-		<?php endif; // End have_posts() check. ?>
+			<?php endif; // End have_posts() check. ?>
 
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php } ?>
+			<?php /* Display navigation to next/previous pages when applicable */ ?>
+			<?php if ( function_exists( 'foundationpress_pagination' ) ) { foundationpress_pagination(); } else if ( is_paged() ) { ?>
+				<nav id="post-nav">
+					<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
+					<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
+				</nav>
+			<?php } ?>
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
+	
 
 	</article>
-	<?php get_sidebar(); ?>
+	
 
 </div>
 
--- sidebar.php
+++ sidebar.php
@@ -8,8 +8,40 @@
  */
 
 ?>
-<aside class="sidebar medium-6 columns hide-for-small">
+<aside class="sidebar medium-6 columns show-for-medium">
 	<?php do_action( 'foundationpress_before_sidebar' ); ?>
-	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
+	<div class="row">
+		<div class="small-24 columns">
+			<div class="recent">
+				<h3>Recent Posts</h3>
+				<?php 
+					$args = array(
+						'posts_per_page' => 5,
+						'post_type' => 'blog'
+					);
+					$query = new WP_Query( $args );
+					if($query->have_posts()) {
+						echo '<ul>';
+						while ($query->have_posts()){
+							$query->the_post();
+							echo '<li><a href="'. get_permalink() .'">' . get_the_title() . '</a></li>';
+
+						}
+						echo '</ul>';
+					} else {
+						//no posts found
+					}
+					/* Restore original Post Data */
+					wp_reset_postdata();
+				?>
+				<a href="<?php echo home_url('/'); ?>blog">All Blog Posts &raquo;</a>
+			</div>
+			<div class="twitter">
+				<h3>Twitter Feed</h3>
+
+			</div>
+		</div>
+	</div>
+	
 	<?php do_action( 'foundationpress_after_sidebar' ); ?>
 </aside>
